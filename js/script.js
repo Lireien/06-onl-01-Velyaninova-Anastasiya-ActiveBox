8f8d0b89d82704bd20bca78e1aa28a02dddb3aaa
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-01-31 19:32:16 +0300

    team slider starts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $('.clients__slider').slick({
         arrows:false,
-        dots: true
+        dots: true,
+        speed:2000,
+        autoplay: true,
+        autoplaySpeed:3000,
     });
+    $('.staff__slider').slick({
+        dots:true,
+        arrows:false,
+        
+    })
 });
