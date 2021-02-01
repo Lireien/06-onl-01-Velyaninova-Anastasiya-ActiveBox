b3765d64630b90861a13ce1b26979882b3bb8760
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-02-01 14:24:30 +0300

    some adaptive for masthead and footer

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.clients__slider').slick({
-        arrows:false,
-        dots: true,
-        speed:2000,
-        autoplay: true,
-        autoplaySpeed:3000,
+    arrows: false,
+    dots: true,
+    speed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     });
     $('.staff__slider').slick({
-        dots:true,
-        arrows:false,
-        
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
     });
     $('.project__slider').slick({
-        arrows:false,
-        dots: true
-    })
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    });
 });
